8fcf25dfe87a4b60cdc0034b71f30dbbc3336dd8
machavesperez <machavesperez@gmail.com>
2018-05-23 23:29:30 +0200

    Incidencias usuario->Apartamento

--- src/main/java/es/uca/iw/Ucapartment/Reserva/ReservaService.java
+++ src/main/java/es/uca/iw/Ucapartment/Reserva/ReservaService.java
@@ -1,7 +1,16 @@
 package es.uca.iw.Ucapartment.Reserva;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.uca.iw.Ucapartment.Estado.Estado;
+import es.uca.iw.Ucapartment.Estado.EstadoRepository;
+import es.uca.iw.Ucapartment.Estado.Valor;
 
 @Service
 public class ReservaService {
@@ -9,9 +18,28 @@ public class ReservaService {
 	@Autowired
 	private ReservaRepository repo;
 	
+	@Autowired
+	private EstadoRepository repoEstado;
+	
 	public Reserva save(Reserva reserva)
 	{
 		return repo.save(reserva);
 	}
+	
+	@Transactional
+	public void pasarelaDePago(double total, Reserva r, Estado e)
+	{
+		double beneficioEmpresa = total * 0.10;
+		double beneficioAnfitrion = total * 0.90;
+		
+		r.setBenenficioEmpresa(beneficioEmpresa);
+		r.setBenenficioAnfitrion(beneficioAnfitrion);
+		e.setValor(Valor.REALIZADA);
+		repoEstado.save(e);
+		repo.save(r);
+		
+	
+	}
+	
 
 }
--- src/main/java/es/uca/iw/Ucapartment/Valoracion/Valoracion.java
+++ src/main/java/es/uca/iw/Ucapartment/Valoracion/Valoracion.java
@@ -7,7 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import es.uca.iw.Ucapartment.Apartamento.Apartamento;
+import es.uca.iw.Ucapartment.Usuario.Usuario;
 
 @Entity//Indica a Hibernate que es una entidad
 @Table(name = "Valoraciones")//Nombre de la tabla en la BD
@@ -20,10 +25,96 @@ public class Valoracion {
 	@Column(length = 512)
 	private String descripcion;
 	
+	private int grado;
+	
 	private Date fecha;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")//Usuario que realiza la valoracion
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_valorado_id")//Usuario valorado
+	private Usuario usuarioValorado;
+	
+	@ManyToOne
+	@JoinColumn(name = "apartamento_valorado_id")//Apartamento valorado
+	private Apartamento apartamentoValorado;
 	
 	//Constructor sin parámetros
 	protected Valoracion() {}
 	
+	//Constructor con parametros, valoracion a usuario
+	public Valoracion(String descripcion, int grado, Date fecha, Usuario usuario,
+		Usuario usuarioValorado) {
+		this.descripcion = descripcion;
+		this.grado = grado;
+		this.fecha = fecha;
+		this.usuario = usuario;
+		this.usuarioValorado = usuarioValorado;
+	}
+	
+	//Constructor con parametros, valoracion a apartamento
+	public Valoracion(String descripcion, int grado, Date fecha, Usuario usuario,
+		Apartamento apartamentoValorado) {
+		this.descripcion = descripcion;
+		this.grado = grado;
+		this.fecha = fecha;
+		this.usuario = usuario;
+		this.apartamentoValorado = apartamentoValorado;
+	}
+
+	//Getters & Setters
+	public Long getId() {
+		return id;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getGrado() {
+		return grado;
+	}
+
+	public void setGrado(int grado) {
+		this.grado = grado;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuarioValorado() {
+		return usuarioValorado;
+	}
+
+	public void setUsuarioValorado(Usuario usuarioValorado) {
+		this.usuarioValorado = usuarioValorado;
+	}
+
+	public Apartamento getApartamentoValorado() {
+		return apartamentoValorado;
+	}
+
+	public void setApartamentoValorado(Apartamento apartamentoValorado) {
+		this.apartamentoValorado = apartamentoValorado;
+	}
 	
 }
