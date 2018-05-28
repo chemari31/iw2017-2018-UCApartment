98a5f8563ad9fc94e83f6224311dd7894810707e
chemari31 <josemaria.garciasanchez@alum.uca.es>
2018-05-28 14:07:27 +0200

    Política de empresa

--- src/main/java/es/uca/iw/Ucapartment/MainScreen.java
+++ src/main/java/es/uca/iw/Ucapartment/MainScreen.java
@@ -3,14 +3,22 @@
  */
 package es.uca.iw.Ucapartment;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 import javax.annotation.PostConstruct;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewDisplay;
 import com.vaadin.server.ExternalResource;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.server.Sizeable.Unit;
 import com.vaadin.spring.annotation.SpringViewDisplay;
+import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
@@ -19,6 +27,7 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.MenuBar.MenuItem;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
@@ -68,6 +77,8 @@ public class MainScreen extends VerticalLayout implements ViewDisplay {
 		Button logoutButton = new Button("Logout", event -> logout());
 		logoutButton.setStyleName(ValoTheme.BUTTON_LINK);
 		//root.addComponent(logoutButton);
+		Button politica = new Button("Política de empresa");
+
 
 		// Creamos la barra de navegación
 		/*final CssLayout navigationBar = new CssLayout();*/
@@ -130,11 +141,19 @@ public class MainScreen extends VerticalLayout implements ViewDisplay {
 
 			menuSuperior.addComponent(new Label("Hola, "+user.getUsername()));
 			menuSuperior.addComponent(logoutButton);
+			politica.addClickListener(event ->{
+				generarpolitica();
+			});
+			menuSuperior.addComponent(politica);
 		}
 		else {
 			
 			menuSuperior.addComponent(createNavigationButton("Iniciar sesión", LoginScreen.VIEW_NAME));
 			menuSuperior.addComponent(createNavigationButton("Registro", RegistroScreen.VIEW_NAME));
+			politica.addClickListener(event ->{
+				generarpolitica();
+			});
+			menuSuperior.addComponent(politica);
 		}
 		
 		root.addComponent(menuSuperior);
@@ -157,6 +176,46 @@ public class MainScreen extends VerticalLayout implements ViewDisplay {
 		return button;
 	}
 	
+	
+	public void generarpolitica()
+	{	
+		try {
+			Document doc = new Document();
+			FileOutputStream fichero = new FileOutputStream("Politica de la empresa.pdf");
+			
+			PdfWriter.getInstance(doc, fichero).setInitialLeading(40);
+			doc.open();
+			
+			doc.add(new Paragraph("Política de nuestra empresa"));
+			doc.add(new Paragraph("_________________________________________________________"));
+			doc.add(new Paragraph("Nuestra empresa sigue las siguientes reglas a la hora de proceder ante una cancelación:"));
+			doc.add(new Paragraph("	- Si se realiza una cancelacíon después de realizar el pago, no se te devolverá nada."));
+			doc.add(new Paragraph(" - Si se cancela una reserva antes de realizar el pago no pasará nada, y el apartamento volverá a estar disponible."));
+			doc.add(new Paragraph(" - Si se modifica la fecha de la reserva se incrementará el precio en esta un 10%."));
+			doc.add(new Paragraph("	- La empresa se quedará un 5% de todas las reservas."));
+			doc.add(new Paragraph("	- El método de pago se realizará mediante una tarjeta de 16 dígitos."));
+			
+			doc.add(new Paragraph("_________________________________________________________"));
+			doc.add(new Paragraph("	La empresa no se hace responsable de los daños ocasionados por los usuarios en los apartamentos. "));
+			doc.add(new Paragraph("	La empresa no se hace responsable de objetos perdidos durante alguna reserva. "));
+			doc.add(new Paragraph("	Y la empresa se reserva el derecho a modificar cualquier dato así de gestionar cualquier apartamento, perfil e incidencia que surga."));
+
+
+
+			
+			doc.close();
+			Notification.show("Política de empresa generada.");
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Notification.show("Se produjo un error generando el documento.");
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Notification.show("Se produjo un error generando el documento.");
+		}
+	}
 	
 	@Override
 	public void showView(View view) {
