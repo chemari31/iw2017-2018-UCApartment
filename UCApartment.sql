a2d14f8bccbe7ccd3f7c91ec10106a534065ffc3
Salva Aragón Reyes <salvaaragon94@gmail.com>
2018-05-28 23:33:26 +0200

    Comando DELETE erroneo

--- UCApartment.sql
+++ UCApartment.sql
@@ -1,4 +1,4 @@
-DELETE DATABASE IF EXISTS Ucapartament;
+DROP DATABASE IF EXISTS Ucapartament;
 CREATE DATABASE Ucapartament DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE Ucapartament;
 CREATE USER IF NOT EXISTS 'springuser'@'localhost' IDENTIFIED BY 'ThePassword';
